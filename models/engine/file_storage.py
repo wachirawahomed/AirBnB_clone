e0620592a80ae4d9eaf6b321f9a496d78bfd5b6c
wachirawahomed <wachirawahomed@gmail.com>
2024-03-11 15:17:40 -0700

    task 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module for FileStorage class"""
+
+import json
+
+class FileStorage:
+    """Serializes instances to JSON file and deserializes JSON file to instances"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to JSON file"""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+                for key, value in obj_dict.items():
+                    cls_name, obj_id = key.split('.')
+                    module = __import__('models.' + cls_name, fromlist=[cls_name])
+                    cls = getattr(module, cls_name)
+                    self.__objects[key] = cls(**value)
+        except FileNotFoundError:
+            pass
