ace9857a8ae671bc0d11208e191510c948ef7b54
wachirawahomed <wachirawahomed@gmail.com>
2024-03-13 07:28:25 +0300

    more

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+State model defines the class state
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+    name = ""
